dc28e97c035208377924c7c3b792668057c23238
Robin <robinnem@outlook.com>
2025-11-03 15:27:25 -0500

    Finished User Posts Practice Project

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListElem = document.querySelector(".user-list");
 
 async function main() {
   const users = await fetch("https://jsonplaceholder.typicode.com/users");
   const usersData = await users.json();
-  const userListElem = document.querySelector(".user-list");
-  userListElem.innerHTML = usersData
-    .map(
-      (user) => `<div class="user-card">
+  userListElem.innerHTML = usersData.map((user) => userHtml(user)).join("");
+}
+
+main();
+
+function showUserPosts(id) {
+  localStorage.setItem("id", id);
+  window.location.href = `${window.location.origin}/user.html`;
+}
+
+function userHtml(user) {
+  return `<div class="user-card" onclick="showUserPosts(${user.id})">
     <div class="user-card__container">
       <h3>${user.name}</h3>
       <p>
@@ -18,14 +27,10 @@ async function main() {
       </p>
       <p>
         <b>Website:</b>
-        <a href="${user.website}" target="_blank">
+        <a href="https://${user.website}" target="_blank">
           ${user.website}
         </a>
       </p>
     </div>
-  </div>`
-    )
-    .join("");
+  </div>`;
 }
-
-main();
