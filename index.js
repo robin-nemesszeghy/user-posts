dc01f0e1dccf2a953e51b34e8268f7f1086042d8
Robin <robinnem@outlook.com>
2025-11-03 13:26:32 -0500

    Calling API for User Data

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+async function main() {
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const usersData = await users.json();
+  const userListElem = document.querySelector(".user-list");
+  userListElem.innerHTML = usersData
+    .map(
+      (user) => `<div class="user-card">
+    <div class="user-card__container">
+      <h3>${user.name}</h3>
+      <p>
+        <b>Email:</b> ${user.email}
+      </p>
+      <p>
+        <b>Phone:</b> ${user.phone}
+      </p>
+      <p>
+        <b>Website:</b>
+        <a href="${user.website}" target="_blank">
+          ${user.website}
+        </a>
+      </p>
+    </div>
+  </div>`
+    )
+    .join("");
+}
+
+main();
